f979310347481ede3545fe6485e2e991fd9d8d1a
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-15 21:54:40 +0530

    feat(ui): scaffold HeroSection component structure

---
+++ components/landing/landing-main.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../ui/button";
+import HeroSection from "./hero-section";
+
+export default function LandingMain() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
