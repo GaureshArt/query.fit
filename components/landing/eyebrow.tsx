30ed7108689a4d364f5fa2c05f9a1f49afc210a3
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-16 20:22:32 +0530

    feat(landing): Add Eyebrow text in hero section

---
+++ components/landing/eyebrow.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+
+interface IEyebrowProps {
+  className?: string;
+  text?: string;
+}
+
+export default function Eyebrow({ text, className }: IEyebrowProps) {
+  return (
+    <div
+      className={cn("flex justify-center-safe items-center-safe", className)}
+    >
+      <h1 className="sr-only">Eyebrow Content - {text}</h1>
+
+      <div
+        className={cn(
+          "min-w-1/3 h-1/2 px-4 py-1 text-md  font-cutive-mono tracking-tighter text-shadow-2xs text-foreground  border rounded border-dashed border-accent flex items-center-safe justify-center gap-4 "
+        )}
+      >
+        <span
+          className={cn(
+            "w-2 h-2  rounded-full  bg-ring shadow-[0px_0px_5px_5px_#58b862ea]"
+          )}
+        ></span>
+        {text}
+      </div>
+    </div>
+  );
+}
