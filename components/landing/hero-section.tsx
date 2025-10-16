63124b6b50e5fd11f713f4c9d48cdc0879df759a
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-16 20:22:32 +0530

    feat(landing): Add Eyebrow text in hero section

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -1,17 +1,20 @@
 import { cn } from "@/lib/utils";
+import Eyebrow from "./eyebrow";
+import { HeroSectionData } from "@/constants/hero-section";
 
 export default function HeroSection() {
   return (
     <>
-      <section className={cn("grid grid-cols-12 grid-rows-12 w-lvw h-lvh")}>
+      <section className={cn("grid grid-cols-12 grid-rows-12 w-lvw h-lvh ")}>
         <h1 id="hero-section-heading" className="sr-only">
           Query Fit Hero Section
         </h1>
         <div
           className={cn(
-            "lg:col-span-8 lg:col-start-3 lg:row-span-8 lg:row-start-3 bg-background"
+            "lg:col-span-8 lg:col-start-3 lg:row-span-8 lg:row-start-3 lg:grid lg:grid-cols-8 lg:grid-rows-8 bg-background"
           )}
         >
+          <Eyebrow className="col-span-full row-span-1 font-dosis " text={HeroSectionData.eyebrowText}/>
 
             
         </div>
---
+++ constants/hero-section.ts
@@ -0,0 +1,7 @@
+export const HeroSectionData = {
+  Heading: "The answers are in your data. Just ask.",
+  subheading:
+    "query.fit translates your plain English questions into precise SQL queries, so you can get instant insights without writing a single line of code.",
+  ctaPrimary: "Talk to data now",
+  eyebrowText: "Talk to your data like never before.",
+};
