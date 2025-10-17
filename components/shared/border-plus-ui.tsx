bc8b56d869671b960056ffcde33f643cf5b48e84
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-18 00:21:56 +0530

    style(landing): Change text and ui of eyebrow in hero section

--- components/shared/border-plus-ui.tsx
+++ components/shared/border-plus-ui.tsx
@@ -6,7 +6,7 @@ export default function BorderPlusUi() {
             <div
                 className={cn(
                     "absolute -right-2 -top-2 font-extralight transition-all duration-200",
-                    "group-hover:-right-1 group-hover:-top-1"
+                    "group-hover:-right-1 group-hover:-top-1 group-hover:rotate-45"
                 )}
             >
                 +
@@ -15,7 +15,7 @@ export default function BorderPlusUi() {
             <div
                 className={cn(
                     "absolute -right-2 -bottom-2 font-extralight transition-all duration-200",
-                    "group-hover:-right-1 group-hover:-bottom-1"
+                    "group-hover:-right-1 group-hover:-bottom-1 group-hover:rotate-45"
                 )}
             >
                 +
@@ -24,7 +24,7 @@ export default function BorderPlusUi() {
             <div
                 className={cn(
                     "absolute -left-2 -top-2 font-extralight transition-all duration-200",
-                    "group-hover:-left-1 group-hover:-top-1"
+                    "group-hover:-left-1 group-hover:-top-1 group-hover:rotate-45"
                 )}
             >
                 +
@@ -33,7 +33,7 @@ export default function BorderPlusUi() {
             <div
                 className={cn(
                     "absolute -left-2 -bottom-2 font-extralight transition-all duration-200",
-                    "group-hover:-left-1 group-hover:-bottom-1"
+                    "group-hover:-left-1 group-hover:-bottom-1 group-hover:rotate-45"
                 )}
             >
                 +
