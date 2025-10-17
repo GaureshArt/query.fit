75ab08637d218873c561714ff7bd19d6e0cac71f
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-17 23:37:17 +0530

    style(landing): update cta  button styles

--- components/landing/cta-button.tsx
+++ components/landing/cta-button.tsx
@@ -1,18 +1,22 @@
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import GoogleIconSvg from "@/public/landing/google-icon";
+import BorderPlusUi from "../shared/border-plus-ui";
 interface ICtaButtonProps {
   className?: string;
   text?: string;
 }
+
+
 export default function CtaButton({ className, text }: ICtaButtonProps) {
   return (
     <div className={cn("flex justify-center items-center gap-2", className)}>
       <Button
         variant={"outline"}
-        className={cn("px-2 py-1 cursor-pointer border-dashed font-semibold hover:bg-accent")}
+        className={cn("group px-2 py-1 cursor-pointer border-0  font-semibold  relative")}
       >
         <GoogleIconSvg />
+        <BorderPlusUi/>
         {text}
       </Button>
     </div>
---
+++ components/shared/border-plus-ui.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+
+export default function BorderPlusUi() {
+    return (
+        <>
+            <div
+                className={cn(
+                    "absolute -right-2 -top-2 font-extralight transition-all duration-200",
+                    "group-hover:-right-1 group-hover:-top-1"
+                )}
+            >
+                +
+            </div>
+
+            <div
+                className={cn(
+                    "absolute -right-2 -bottom-2 font-extralight transition-all duration-200",
+                    "group-hover:-right-1 group-hover:-bottom-1"
+                )}
+            >
+                +
+            </div>
+
+            <div
+                className={cn(
+                    "absolute -left-2 -top-2 font-extralight transition-all duration-200",
+                    "group-hover:-left-1 group-hover:-top-1"
+                )}
+            >
+                +
+            </div>
+
+            <div
+                className={cn(
+                    "absolute -left-2 -bottom-2 font-extralight transition-all duration-200",
+                    "group-hover:-left-1 group-hover:-bottom-1"
+                )}
+            >
+                +
+            </div>
+        </>
+    );
+}
