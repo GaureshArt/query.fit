7b0199689bbc061e26ec6a6fc42368a6b25e792a
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-18 00:21:56 +0530

    style(landing): Change text and ui of eyebrow in hero section

--- components/landing/eyebrow.tsx
+++ components/landing/eyebrow.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 
+
 interface IEyebrowProps {
   className?: string;
   text?: string;
@@ -7,22 +8,23 @@ interface IEyebrowProps {
 
 export default function Eyebrow({ text, className }: IEyebrowProps) {
   return (
-    <div
-      className={cn("flex justify-center-safe items-center-safe ", className)}
-    >
+    <div className={cn("flex justify-center items-center", className)}>
       <h1 className="sr-only">Eyebrow Content - {text}</h1>
 
       <div
         className={cn(
-          "min-w-1/3 h-1/2 px-4 py-1 text-md  font-cutive-mono tracking-tighter text-shadow-2xs text-foreground  border rounded-md border-white/20   flex items-center-safe justify-center gap-4 "
+          "flex items-center justify-center gap-3",
+          "px-4 py-1.5",
+          "font-cutive-mono tracking-wide text-foreground text-sm",
+          "border border-y-0  border-neutral-700",
+          "rounded-sm"
         )}
       >
         <span
-          className={cn(
-            "w-2 h-2  rounded-full  bg-ring shadow-[0px_0px_5px_5px_#58b862ea]"
-          )}
+          className={cn("w-2 h-2 rounded-full bg-green-500 animate-pulse")}
         ></span>
         {text}
+        
       </div>
     </div>
   );
