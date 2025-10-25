6844a08bc10d38b7a8af63d62d598cebee684c1d
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-25 23:54:08 +0530

    feat(auth): Implement Google social auth with supabase.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import LandingMain from "@/components/landing/landing-main";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient()
+  const user = await supabase.auth.getUser();
+      if(user.data.user?.id){
+          redirect('/settings')
+      }
   return (
     <>
       <LandingMain />
