54795ea7c6537322466a7135ce3beaf4cd4a5e45
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-25 19:25:10 +0530

    style(landing):Change style of Hero heading in hero section

--- constants/hero-section.ts
+++ constants/hero-section.ts
@@ -1,4 +1,5 @@
 export const HeroSectionData = {
+  heroHeading:"The answers are in your data. Just ask.",
   subheading:
     "query.fit translates your plain English questions into precise SQL queries, so you can get instant insights without writing a single line of code.",
   ctaPrimary: "Talk to Data NOW",
