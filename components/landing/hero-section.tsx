f6d58c3b6e9b9269a7e0bb37fd250d9f2064f38c
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-25 19:55:52 +0530

    style(landing): Change border style of full Hero section

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -4,25 +4,31 @@ import { HeroSectionData } from "@/constants/hero-section";
 import HeroHeading from "./hero-heading";
 import HeroSubheading from "./hero-subheading";
 import CtaButton from "./cta-button";
+import BorderPlusUi from "../shared/border-plus-ui";
+import PlusSvg from "../shared/plus-svg";
 
 export default function HeroSection() {
   return (
     <>
-      <section className={cn("grid grid-cols-12 grid-rows-12 w-lvw h-lvh ")}>
+      <section className={cn("grid grid-cols-12 grid-rows-12 w-lvw h-lvh  ")}>
         <h1 id="hero-section-heading" className="sr-only">
           Query Fit Hero Section
         </h1>
 
         <div
           className={cn(
-            "lg:col-span-8 lg:col-start-3 lg:row-span-8 lg:row-start-3 lg:grid lg:grid-cols-8 lg:grid-rows-8 bg-background"
+            "relative lg:col-span-8 lg:col-start-3 lg:row-span-8 lg:row-start-3 lg:grid lg:grid-cols-8 lg:grid-rows-8 bg-background border border-zinc-500/20 "
           )}
         >
+          <PlusSvg/>
           <Eyebrow
             className="col-span-full row-span-1 font-dosis "
             text={HeroSectionData.eyebrowText}
           />
-          <HeroHeading className="col-span-4 row-span-4 col-start-3 row-start-2 font-dosis" text={HeroSectionData.heroHeading}/>
+          <HeroHeading
+            className="col-span-4 row-span-4 col-start-3 row-start-2 font-dosis"
+            text={HeroSectionData.heroHeading}
+          />
           <HeroSubheading
             className="col-span-6 col-start-2 row-span-1 row-start-6 font-cutive-mono "
             text={HeroSectionData.subheading}
---
+++ components/shared/plus-svg.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+
+export default function PlusSvg() {
+  return (
+    <>
+    
+    <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
+        "stroke-zinc-900 absolute -left-4 -top-4"
+    )}>
+      <g fill="none" opacity="0.4">
+        <path d="M48 23.5L0 23.5" ></path>
+        <path d="M48 47.5001L0 " ></path>
+        <path d="M23.5 0V48" ></path>
+        <path d="M47.5 " ></path>
+      </g>
+    </svg>
+    <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
+        "stroke-zinc-900 absolute -right-4 -top-4"
+    )}>
+      <g fill="none" opacity="0.4">
+        <path d="M48 23.5L0 23.5" ></path>
+        <path d="M48 47.5001L0 " ></path>
+        <path d="M23.5 0V48" ></path>
+        <path d="M47.5 " ></path>
+      </g>
+    </svg>
+    <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
+        "stroke-zinc-900 absolute -left-4 -bottom-4"
+    )}>
+      <g fill="none" opacity="0.4">
+        <path d="M48 23.5L0 23.5" ></path>
+        <path d="M48 47.5001L0 " ></path>
+        <path d="M23.5 0V48" ></path>
+        <path d="M47.5 " ></path>
+      </g>
+    </svg>
+    <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
+        "stroke-zinc-900 absolute -right-4 -bottom-4"
+    )}>
+      <g fill="none" opacity="0.4">
+        <path d="M48 23.5L0 23.5" ></path>
+        <path d="M48 47.5001L0 " ></path>
+        <path d="M23.5 0V48" ></path>
+        <path d="M47.5 " ></path>
+      </g>
+    </svg>
+    </>
+  );
+}
