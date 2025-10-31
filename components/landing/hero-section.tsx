b9479abf7815ca988999650d536661778dfd5222
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-10-31 22:39:45 +0530

    feat(auth): set up subscription table and free plan provisioning

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -4,19 +4,15 @@ import { HeroSectionData } from "@/constants/hero-section";
 import HeroHeading from "./hero-heading";
 import HeroSubheading from "./hero-subheading";
 import CtaButton from "./cta-button";
-import BorderPlusUi from "../shared/border-plus-ui";
-import PlusSvg from "../shared/plus-svg";
+import PlusSvg from "@/components/shared/plus-svg";
 import { createClient } from "@/utils/supabase/server";
-import { Button } from "../ui/button";
 import GoogleIconSvg from "@/public/landing/google-icon";
 import { signInWithGoogle } from "@/utils/supabase/actions";
-
-import WelcomeButton from "./welcome-button";
-
+import WelcomeButton from "@/components/landing/welcome-button";
 export default async function HeroSection() {
   const supabase = await createClient();
   const user = await supabase.auth.getUser();
-  
+
   return (
     <>
       <section className={cn("grid grid-cols-12 grid-rows-12 w-lvw h-lvh  ")}>
@@ -45,20 +41,21 @@ export default async function HeroSection() {
           {user.data.user?.id ? (
             <div className={cn("col-span-2 flex justify-center col-start-4")}>
               <WelcomeButton
-               text={`Welcome ${user.data.user.user_metadata.name.split(" ")[0]}`}
-               iconSvg={null}
-               
-                className={cn(" row-span-1 row-start-7  font-cutive-mono cursor-pointer")}
+                text={`Welcome ${
+                  user.data.user.user_metadata.name.split(" ")[0]
+                }`}
+                iconSvg={null}
+                className={cn(
+                  " row-span-1 row-start-7  font-cutive-mono cursor-pointer"
+                )}
               />
-            
-            
             </div>
           ) : (
             <CtaButton
               className="col-span-full row-span-1 row-start-7 font-cutive-mono"
               text={HeroSectionData.ctaPrimary}
               clickFunc={signInWithGoogle}
-              iconSvg={ <GoogleIconSvg />}
+              iconSvg={<GoogleIconSvg />}
             />
           )}
         </div>
