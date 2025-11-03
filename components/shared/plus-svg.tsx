90bac859fdbb5dae6078eb82d52658f13e35d79f
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-11-03 11:37:36 +0530

    feat(workspace): build file upload ui with shadcn field and zod validation

--- components/shared/plus-svg.tsx
+++ components/shared/plus-svg.tsx
@@ -1,13 +1,17 @@
 import { cn } from "@/lib/utils";
 
-export default function PlusSvg() {
+
+interface IPlusSvgProps{
+  opacity?:number
+}
+export default function PlusSvg({opacity = 0.4}:IPlusSvgProps) {
   return (
     <>
     
     <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
         "stroke-zinc-900 absolute -left-4 -top-4"
     )}>
-      <g fill="none" opacity="0.4">
+      <g fill="none" opacity={opacity}>
         <path d="M48 23.5L0 23.5" ></path>
         <path d="M48 47.5001L0 " ></path>
         <path d="M23.5 0V48" ></path>
@@ -17,7 +21,7 @@ export default function PlusSvg() {
     <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
         "stroke-zinc-900 absolute -right-4 -top-4"
     )}>
-      <g fill="none" opacity="0.4">
+      <g fill="none" opacity={opacity}>
         <path d="M48 23.5L0 23.5" ></path>
         <path d="M48 47.5001L0 " ></path>
         <path d="M23.5 0V48" ></path>
@@ -27,7 +31,7 @@ export default function PlusSvg() {
     <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
         "stroke-zinc-900 absolute -left-4 -bottom-4"
     )}>
-      <g fill="none" opacity="0.4">
+      <g fill="none" opacity={opacity}>
         <path d="M48 23.5L0 23.5" ></path>
         <path d="M48 47.5001L0 " ></path>
         <path d="M23.5 0V48" ></path>
@@ -37,7 +41,7 @@ export default function PlusSvg() {
     <svg width="32" height="32" viewBox="0 0 48 48" className={cn(
         "stroke-zinc-900 absolute -right-4 -bottom-4"
     )}>
-      <g fill="none" opacity="0.4">
+      <g fill="none" opacity={opacity}>
         <path d="M48 23.5L0 23.5" ></path>
         <path d="M48 47.5001L0 " ></path>
         <path d="M23.5 0V48" ></path>
