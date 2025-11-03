538252288c4d1295f3bb6209d2537de869e2d514
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-11-03 23:36:49 +0530

    feat(workspace): Build chat input form form with validation

--- app/(app)/workspace/query/page.tsx
+++ app/(app)/workspace/query/page.tsx
@@ -1,8 +1,9 @@
+import QueryInterface from "@/components/app/workspace/query-interface";
+
 export default function Page(){
     return (
-        <div className="bg-red-500">
-
-            Hellow Work space query chat
+        <div className="px-8 py-4 w-full h-full flex justify-center">
+            <QueryInterface/>
         </div>
     )
 }
