7b67c2b9541b2e35b56217ab06d73a6015067314
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-11-16 13:29:44 +0530

    feat(agent): add agentic nodes structure to give agentic behaviour

--- utils/agent/state.ts
+++ utils/agent/state.ts
@@ -1,29 +1,41 @@
-import * as z from "zod"
-import {BaseMessage} from "@langchain/core/messages"
-import { registry } from "@langchain/langgraph/zod"
+import * as z from "zod";
+import { BaseMessage } from "@langchain/core/messages";
+import { registry } from "@langchain/langgraph/zod";
 import { queryPlannerLlmSchema } from "./models";
 export const graphState = z.object({
-    messages:z.array(z.custom<BaseMessage>()).register(registry,{
-       reducer:{
-            fn:(a:BaseMessage[],b:BaseMessage[])=>a.concat(b)
-        },
-        default:()=>[] as BaseMessage[]
-    }),
-    dbId:z.string(),
-    schema: z.string().optional(),
-    sqlQuery: z.string().optional(),
-    queryResult: z.any().optional(),
-    currentStepIndex:z.number().default(0),
-    routeDecision:z.string().optional(),
-    lastError:z.string().optional(),
-    retryCount:z.number().default(0),
-    needsReplanning: z.boolean().default(false),
-    chartSpec:z.any().optional(),
-    answeredQuery:z.boolean().optional(),
-    queryPlan:z.custom<z.infer<typeof queryPlannerLlmSchema>>().optional()
-})
+  messages: z.array(z.custom<BaseMessage>()).register(registry, {
+    reducer: {
+      fn: (a: BaseMessage[], b: BaseMessage[]) => a.concat(b),
+    },
+    default: () => [] as BaseMessage[],
+  }),
+  dbId: z.string(),
+  schema: z.string().optional(),
+  sqlQuery: z.string().optional(),
+  queryResult: z.any().optional(),
+  currentStepIndex: z.number().default(0),
+  routeDecision: z
+    .enum([
+      "generateSchema",
+      "generateQuery",
+      "queryClarifier",
+      "executeQuery",
+      "summarizeOutput",
+      "complexQueryApproval",
+      "queryPlanner",
+      "generalChat",
+      "validator",
+      "orchestrator",
+      "__end__"
+    ])
+    .optional(),
+  lastError: z.string().optional(),
+  retryCount: z.number().default(0),
+  needsReplanning: z.boolean().default(false),
+  chartSpec: z.any().optional(),
+  answeredQuery: z.boolean().optional(),
+  feedback: z.string().optional(),
+  queryPlan: z.custom<z.infer<typeof queryPlannerLlmSchema>>().optional(),
+});
 
 export type GraphState = z.infer<typeof graphState>;
-
-
-
