c20af828e1705c4458b426c651fe1b8b4fd716e6
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-12-24 11:03:29 +0530

    style(settings): add icons to the settings sub nav

--- constants/setting-sub-nav.ts
+++ constants/setting-sub-nav.ts
@@ -1,10 +1,14 @@
+import QuerySvg from "@/public/setting-svgs/query-svg"
+import AiProviderSvg from "@/public/setting-svgs/ai-provider-svg"
+import DatabaseSvg from "@/public/setting-svgs/database-svg"
 export const SETTINGS_SUB_NAV = [
   {
     id: 1,
     name: "AI Providers",
     link: "/settings/ai-providers",
     description:
-      "Manage API keys, default models, temperature, and fallback providers for query generation."
+      "Manage API keys, default models, temperature, and fallback providers for query generation.",
+    logo:AiProviderSvg
   },
   {
     id: 2,
@@ -12,12 +16,15 @@ export const SETTINGS_SUB_NAV = [
     link: "/settings/database",
     description:
       "Configure guardrails to prevent unsafe SQL like destructive queries and enforce read-only rules."
+      ,
+      logo:DatabaseSvg
   },
   {
     id: 3,
     name: "Query Generation",
     link: "/settings/query",
     description:
-      "Control how queries are generated, retried, explained, and validated before execution."
+      "Control how queries are generated, retried, explained, and validated before execution.",
+      logo:QuerySvg
   }
 ];
---
+++ public/setting-svgs/database-svg.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+
+export default function DatabaseSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="24"
+      height="24"
+      viewBox="0 0 20 20"
+    >
+      <path
+        d="m3.102,10.492c.549,1.422,3.424,2.508,6.898,2.508s6.349-1.086,6.898-2.508"
+        fill="none"
+        stroke="rgba(34, 34, 34, 0.83)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        data-color="color-2"
+      ></path>
+      <path
+        d="m3.102,14.492c.549,1.422,3.424,2.508,6.898,2.508s6.349-1.086,6.898-2.508"
+        fill="none"
+        stroke="rgba(34, 34, 34, 0.83)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        data-color="color-2"
+      ></path>
+      <ellipse
+        cx="10"
+        cy="6"
+        rx="7"
+        ry="3"
+        stroke="rgba(34, 34, 34, 0.83)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        fill="rgba(34, 34, 34, 0.83)"
+      ></ellipse>
+    </svg>
+  );
+}
